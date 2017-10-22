3f39ad821fb17b21b708321d60c4916c2c027aec
WulanR <wulanr161299@gmail.com>
2017-10-22 11:37:49 +0700

    Add Custom Marker

--- app/src/main/java/id/sch/smktelkom_mlg/student/googlemaps/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/student/googlemaps/MainActivity.java
@@ -9,47 +9,85 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     static final CameraPosition NEWYORK = CameraPosition.builder()
             .target(new LatLng(40.7127, 74.0059))
-            .zoom(21)
+            .zoom(15)
             .bearing(0)
-            .tilt(45)
-            .build();
-    static final CameraPosition SMKTELKOM = CameraPosition.builder()
-            .target(new LatLng(-7.9768972, 112.6565519))
-            .zoom(21)
-            .bearing(0)
-            .tilt(45)
+            .tilt(0)
             .build();
     static final CameraPosition KUALALUMPUR = CameraPosition.builder()
             .target(new LatLng(3.1385036, 101.6169484))
-            .zoom(21)
+            .zoom(15)
             .bearing(0)
-            .tilt(45)
+            .tilt(0)
             .build();
+    static final CameraPosition MALANG = CameraPosition.builder()
+            .target(new LatLng(-7.97683, 112.6586844))
+            .zoom(15)
+            .bearing(0)
+            .tilt(0)
+            .build();
+    MarkerOptions smktelkom;
+    MarkerOptions kosrocky;
+    MarkerOptions kosdorayaki;
+    MarkerOptions bebekdulur;
+    MarkerOptions nelongso;
+    MarkerOptions biru;
     GoogleMap m_map;
     boolean mapReady = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        smktelkom = new MarkerOptions()
+                .position(new LatLng(-7.9779684, 112.6592985))
+                .title("SMK Telkom")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
 
-        Button btnMap = (Button) findViewById(R.id.btnTelkom);
-        btnMap.setOnClickListener(new View.OnClickListener() {
+        kosrocky = new MarkerOptions()
+                .position(new LatLng(-7.977883, 112.658810))
+                .title("Kos Rocky")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
+
+        kosdorayaki = new MarkerOptions()
+                .position(new LatLng(-7.977683, 112.658922))
+                .title("Kos Dorayaki")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
+
+        bebekdulur = new MarkerOptions()
+                .position(new LatLng(-7.974750, 112.660070))
+                .title("Bebek Dulur")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
+
+        nelongso = new MarkerOptions()
+                .position(new LatLng(-7.976780, 112.663762))
+                .title("Ayam Nelongso")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
+
+        biru = new MarkerOptions()
+                .position(new LatLng(7.977464, 112.654484))
+                .title("Biru")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
+
+        Button btnMalang = (Button) findViewById(R.id.btnMalang);
+        btnMalang.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (mapReady)
-                    flyTo(SMKTELKOM);
+                    flyTo(MALANG);
             }
         });
 
-        Button btnSatelit = (Button) findViewById(R.id.btnNewYork);
-        btnSatelit.setOnClickListener(new View.OnClickListener() {
+        Button btnNewYork = (Button) findViewById(R.id.btnNewYork);
+        btnNewYork.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (mapReady)
@@ -57,8 +95,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         });
 
-        Button btnHybrid = (Button) findViewById(R.id.btnMalaysia);
-        btnHybrid.setOnClickListener(new View.OnClickListener() {
+        Button btnKuala = (Button) findViewById(R.id.btnKuala);
+        btnKuala.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (mapReady)
@@ -71,14 +109,18 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     private void flyTo(CameraPosition target) {
-        m_map.animateCamera(CameraUpdateFactory.newCameraPosition(target), 10000, null);
+        m_map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
     }
 
     @Override
     public void onMapReady(GoogleMap map) {
         mapReady = true;
         m_map = map;
-        m_map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-        flyTo(SMKTELKOM);
+        m_map.addMarker(smktelkom);
+        m_map.addMarker(kosrocky);
+        m_map.addMarker(kosdorayaki);
+        m_map.addMarker(bebekdulur);
+        m_map.addMarker(biru);
+        flyTo(MALANG);
     }
 }
